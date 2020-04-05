8ed2e12c5847ddda4bfefb0cdb59835aab88bec2
Akshat Shah <aksshah2004@gmail.com>
2020-04-05 12:18:08 -0500

    JavaFX TodoList application complete, working with styling now

---
+++ Java/JavaFX Application/src/sample/Controller.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button button4;
+
+    public void initialize() {
+        button4.setEffect(new DropShadow());
+    }
+
+    @FXML
+    public void handleMouseEnter() {
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit() {
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
+}
